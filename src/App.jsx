274e9d62ee12c144ddff4805c3eff94f122af816
RakanGarvey <122347903+RakanGarvey@users.noreply.github.com>
2023-12-13 18:45:49 +0100

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 // import Header from './components/Header'
+import { Link } from "react-scroll";
 import Home from './components/Home'
 import About from './components/About'
 import Portofolio from './components/Portofolio'
@@ -36,12 +37,12 @@ function App() {
       <Contact id='contact'/>
       <footer>
         <div className="ul1">
-        <ul className='ul'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Portofolio</li>
-          <li>Contact</li>
+        <ul className='ul0'>
+            <Link className='li' to='home' duration={1000} smooth={true}>Home</Link>
+            <Link className='li' to='about' duration={1000} smooth={true}>About</Link>
+            <Link className='li' to='services' duration={1000} smooth={true}>Services</Link>
+            <Link className='li' to='portofolio' duration={1000} smooth={true}>Portofolio</Link>
+            <Link className='li' to='contact' duration={1000} smooth={true}>Contact</Link>
         </ul>
         </div>
         <div className='last'>
