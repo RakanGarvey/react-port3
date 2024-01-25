f8f2590ff007d1ea7c5dd22600be23444c7bb346
Simo Hamed <122347903+SIMOHAMEDdev@users.noreply.github.com>
2024-01-25 17:20:13 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,11 @@ const Header = () => {
     <div className='home1'>
         <img src={logo} alt="" className='logo'/>
         <ul className='ul0'>
-            <Link className='li' to='home' duration={1000} smooth={true}>Home</Link>
-            <Link className='li' to='about' duration={1000} smooth={true}>About</Link>
-            <Link className='li' to='services' duration={1000} smooth={true}>Services</Link>
-            <Link className='li' to='portofolio' duration={1000} smooth={true}>Portofolio</Link>
-            <Link className='li' to='contact' duration={1000} smooth={true}>Contact</Link>
+            <Link className='li' to='home' duration={600} smooth={true}>Home</Link>
+            <Link className='li' to='about' duration={600} smooth={true}>About</Link>
+            <Link className='li' to='services' duration={600} smooth={true}>Services</Link>
+            <Link className='li' to='portofolio' duration={600} smooth={true}>Portofolio</Link>
+            <Link className='li' to='contact' duration={600} smooth={true}>Contact</Link>
         </ul>
         <Menu />
     </div>
